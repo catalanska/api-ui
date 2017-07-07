b7a3c2116bc847fbbe22f5530fa019a8e7d93dbe
javierjv <javier@digitalorigin.com>
2017-07-07 18:02:55 +0200

    execute after load

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import './main.css';
 import { getMethodsList } from './utils/api';
 import ApiMethod from './apiMethod';
 
-getMethodsList().then((methodsList) => {
-  methodsList.forEach((endpoint) => {
-    const apiMethod = new ApiMethod(endpoint);
-    apiMethod.render();
-    apiMethod.fetchData();
+
+const init = () => {
+  getMethodsList().then((methodsList) => {
+    methodsList.forEach((endpoint) => {
+      const apiMethod = new ApiMethod(endpoint);
+      apiMethod.render();
+      apiMethod.fetchData();
+    });
   });
-});
+};
+
+/**
+ * Adds an event listener to window &
+ * executes code after all assets have been loaded
+ */
+window.addEventListener('load', init, false);
