c1150973104df507195d4693f134ac48bcf96d3f
javierjv <javier@digitalorigin.com>
2017-07-07 10:03:03 +0200

    returning mock data read from file

--- server/index.js
+++ server/index.js
@@ -1,48 +1,42 @@
 const express = require('express');
+const { getMockData } = require('../src/utils/mockData');
 
-const app = express();
-
-const publicMethods = [
-  {
-    name: 'Advertiser List',
-    url: 'advertisers',
-    method: 'GET',
-  },
-];
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-app.get('/listOfMethods', (req, res) => {
-  res.json(publicMethods);
-});
-
-app.get('/advertisers', (req, res) => {
-  res.format({
-    'text/xml': () => {
-      res.send('<p>hey</p>');
+getMockData().then(({ jsonResponse, xmlResponse }) => {
+  const app = express();
+  const publicMethods = [
+    {
+      name: 'Advertiser List',
+      url: 'advertisers',
+      method: 'GET',
     },
+  ];
 
-    'application/json': () => {
-      res.send([
-        {
-          id: 1,
-          name: 'MockAdvertiser',
-          external_id: '1',
-          impressions: 52721284,
-          clicks: 93149,
-        },
-      ]);
-    },
-
-    default: () => {
-      // only json or xml are allowed
-      res.status(406).send('Not Acceptable');
-    },
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
   });
-});
 
-app.listen(3000);
+  app.get('/listOfMethods', (req, res) => {
+    res.json(publicMethods);
+  });
+
+  app.get('/advertisers', (req, res) => {
+    res.format({
+      'text/xml': () => {
+        res.send(xmlResponse);
+      },
+
+      'application/json': () => {
+        res.send(jsonResponse);
+      },
+
+      default: () => {
+        // only json or xml are allowed
+        res.status(406).send('Not Acceptable');
+      },
+    });
+  });
+
+  app.listen(3000);
+});
